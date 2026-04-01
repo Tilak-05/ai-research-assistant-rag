adf880ce17046437a53e235e529b1bc036a34527
Tilak-05 <tilakluniya@gmail.com>
2026-04-01 12:28:31 +0530

    update

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
+
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.getcwd())
 
 import streamlit as st
 from langchain_community.vectorstores import FAISS
